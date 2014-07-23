423ee0495007a6485cb330429c7724a5fc0aa901
Gabriel Erion <gerion@rclogin04.rc.fas.harvard.edu>
2014-07-23 00:24:46 -0400

    correct setup.py for building on remote

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 import numpy as np
 
 extensions = [
-	Extension('tvti', ['tt.pyx'],
+	Extension('tt', ['tt.pyx'],
 		include_dirs=[np.get_include()]),
 	Extension('impute', ['impute.pyx'],
 		include_dirs=[np.get_include()]),

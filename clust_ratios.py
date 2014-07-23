18226c069f5ced3fe4fc6bf4e131b45dec3f1a1b
Gabe Erion <gabe.erion@gmail.com>
2014-07-23 00:56:30 -0400

    simpler clust_ratios.py

--- clust_ratios.py
+++ clust_ratios.py
@@ -1,11 +1,8 @@
-import mcmc as m, tests as t
+import mcmc as m
 import numpy as np
-import impute, tt
-bwg = np.array([[c for c in s.seq] for s in m.AlignIO.read('pol-global.fasta', 'fasta') if 'BW' in s.id])
-import scratch as s
-bwg = s.arint(bwg)
-import random
-subs = [np.array(random.sample(bwg,40)) for i in xrange(100)]
-subclusts = map(t.clust,subs)
-d = subs[np.where(subclusts==np.median(subclusts))[0][0]]
-m.lclass(d,29)
+
+DFILE = 'd_clust.csv'
+IMPS = 29
+
+d = np.genfromtxt(DFILE, delimiter=',').astype(np.int)
+m.lclass(d,IMPS)
